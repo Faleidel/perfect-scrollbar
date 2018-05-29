5ef525b1b436def9b21ec08d7903114398a6cc32
Faleidel <raphaelsimeon@gmail.com>
2018-05-29 13:57:41 -0400

    Added support for rail drag on mobile

--- src/handlers/drag-thumb.js
+++ src/handlers/drag-thumb.js
@@ -61,11 +61,28 @@ function bindMouseScrollHandler(
     e.stopPropagation();
     e.preventDefault();
   }
+  function touchMoveHandler(e) {
+    var eps = { pageX : e.targetTouches[0].pageX
+              , pageY : e.targetTouches[0].pageY
+              };
+    
+    element[scrollTop] =
+      startingScrollTop + scrollBy * (eps[pageY] - startingMousePageY);
+    addScrollingClass(i, y);
+    updateGeometry(i);
+
+    e.stopPropagation();
+  }
 
   function mouseUpHandler() {
     removeScrollingClass(i, y);
     i[scrollbarYRail].classList.remove(cls.state.clicking);
     i.event.unbind(i.ownerDocument, 'mousemove', mouseMoveHandler);
+  }
+  function touchEndHandler() {
+    removeScrollingClass(i, y);
+    i[scrollbarYRail].classList.remove(cls.state.clicking);
+    i.event.unbind(i.ownerDocument, 'touchmove', mouseMoveHandler);
   }
 
   i.event.bind(i[scrollbarY], 'mousedown', e => {
@@ -82,5 +99,24 @@ function bindMouseScrollHandler(
 
     e.stopPropagation();
     e.preventDefault();
+  });
+  i.event.bind(i[scrollbarY], 'touchstart', e => {
+    startingScrollTop = element[scrollTop];
+    
+    var eps = { pageX : e.targetTouches[0].pageX
+              , pageY : e.targetTouches[0].pageY
+              };
+    
+    startingMousePageY = eps[pageY];
+    scrollBy =
+      (i[contentHeight] - i[containerHeight]) /
+      (i[railYHeight] - i[scrollbarYHeight]);
+
+    i.event.bind(i.ownerDocument, 'touchmove', touchMoveHandler);
+    i.event.once(i.ownerDocument, 'touchend', touchEndHandler);
+
+    i[scrollbarYRail].classList.add(cls.state.clicking);
+
+    e.stopPropagation();
   });
 }
